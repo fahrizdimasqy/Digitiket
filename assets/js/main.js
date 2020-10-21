f176407e9a57b50257870e0f6babfa0db2a5afdd
fahrizdimasqy <1197050037@student.uinsgd.ac.id>
2020-10-21 11:55:43 +0700

    done all version but have problem with card tranding margin

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,21 +35,58 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function () {
-    $(".card-tranding-carousel").owlCarousel({
-        loop: true,
-        margin: 10,
-        dots: true,
-        autoplay:true,    
-        responsive:{
-            300:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
+// $(document).ready(function () {
+//     $(".card-tranding-carousel").owlCarousel({
+//         loop: true,
+//         margin: 10,
+//         dots: true,
+//         autoplay:true,    
+//         responsive:{
+//             300:{
+//                 items:1
+//             },
+//             1000:{
+//                 items:1
+//             }
+//         }
+//     });
+// });
+
+$('.card-tranding-carousel').slick({
+    dots: true,
+    autoplay:true,
+    slidesPerRow: 2,
+    rows: 2,
+    margin:0,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesPerRow: 1,
+            row: 0
+          }
         }
-    });
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
 });
 
 // $('.single-item').slick({
